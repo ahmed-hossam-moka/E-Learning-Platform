762d04dbb36c2d3f3fecdcf54e7806c4afd176dd
Marwan Mohammed <168595917+Borhy0@users.noreply.github.com>
2025-04-26 23:59:24 +0300

    Delete OnlineLearningPlatform.DAL directory

--- OnlineLearningPlatform.DAL/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OnlineLearningPlatform.DAL.Models
-{
-    public class Category
-    {
-        public int CategoryId { get; set; }
-        public string Name { get; set; }
-        public ICollection<Course> Courses { get; set; }
-    }
-}
