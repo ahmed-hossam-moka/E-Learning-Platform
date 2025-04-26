90cd8b704fdbf0688824ffede02202720ca857ab
Marwan Mohammed <168595917+Borhy0@users.noreply.github.com>
2025-04-26 23:50:50 +0300

    Add files via upload
    FIXING the repo for the course and category

---
+++ OnlineLearningPlatform.DAL/ICategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineLearningPlatform.DAL.DataBase;
+using OnlineLearningPlatform.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLearningPlatform.DAL.Repository
+{
+    public interface ICategoryRepository
+    {
+        public IQueryable<Category> GetAll();
+        public Category GetById(int Id);
+        public void Add(Category category);
+        public void Update(Category category);
+        public void Delete(Category category);
+    }
+}
